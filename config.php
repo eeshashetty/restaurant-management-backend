9e1d3f1fee3fb551e5121a1dfdeec7de843b7515
Eesha Shetty <ekshetty2410@gmail.com>
2020-07-19 18:38:24 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'localhost:3306');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'eeshasql');
-   define('DB_DATABASE', 'database');
+   define('DB_SERVER', '');
+   define('DB_USERNAME', '');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', '');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
